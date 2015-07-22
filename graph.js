6d1e1fb70bd3e86abd37d36329ae1c08d61a5d0a
matan <dev.matan@gmail.com>
2015-07-22 23:14:18 +0300

    "load data on page load, using d3 ajax and parsing"

--- graph.js
+++ graph.js
@@ -1,13 +1,81 @@
 console.log("javascript started")
 
-var g = new dagre.graphlib.Graph();
+var graph = new dagre.graphlib.Graph();
+graph.setGraph({});
 
-g.setGraph({});
+function getNodes(callback){
+  console.log('loading nodes')
+  d3.csv('cae-data/nodes', function(err, inputNodes) {
+    if (err) console.error(err)
+    else {
+      console.log("input nodes: "); console.dir (inputNodes)
+      inputNodes.forEach(function(node) {
+        // TODO: get bounding box size required for rendering the label 
+        graph.setNode(node.id, { name: node.name, kind: node.kind, width:10, height:10 })
+      })
+      console.log("nodes: "); console.dir(graph.nodes())
+      
+      console.log('loading sources, this may take a while'); getSources(callback)
+    }
+  })
+}
+
+function getEdges(callback){
+  console.log('loading edges')
+  d3.csv('cae-data/edges', function(err, inputEdges) {
+    if (err) console.error(err)
+    else {
+      console.log("input edges: "); console.dir(inputEdges)
+      inputEdges.forEach(function(edge) {
+        // TODO: get bounding box size required for rendering the label 
+        graph.setEdge(edge.id1, edge.id2, { edgeKind: edge.edgeKind });
+      })
+      console.log("edges: "); console.dir(graph.edges())
+
+      inputEdges.forEach(function(edge) {
+        if (graph.edge({v:edge.id1, w:edge.id2}) === undefined)
+          console.warn("input edge " + edge  + " failed to initialize as a graphlib edge")
+      })
+
+      callback()
+    }
+  })
+}
+
+sourceMap = {}
+
+// recursively fetch source for all nodes, synchronously
+function getSources(callback, i) {
+  i = i+1 || 0; if (i == graph.nodes().length) callback()
+  else {
+    id = graph.nodes()[i]
+    d3.text('cae-data/' + 'node-source-' + id, function(err, nodeSource) {
+      if (err) console.error(err)
+      else {
+        sourceMap[id] = nodeSource
+        getSources(callback, i) // next
+      }
+    })
+  }
+}
+
+function verifyDataLoad(callback) {
+  if (Object.keys(sourceMap).length != graph.nodes().length)
+    console.warn('number of sources does not equal the number of nodes')
+  else 
+    console.log('data loading done')
+}
+
+function fetchData(callback) {
+  // callback-hell-style flow control for all data loading
+  getNodes(function(){getEdges(verifyDataLoad)})
+}
 
 
-
+fetchData()
 
 //dagre.layout(g);
+
 //console.log("layout computed")
 
 
@@ -22,7 +90,7 @@ g.edges().forEach(function(e) {
 */
 
 // validate the input data
-if (g.nodes().filter(
+if (graph.nodes().filter(
   function(node) {
   return g.node(node) === undefined
 }).length > 0)
